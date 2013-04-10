79de35747c2d0881fc3963aa8858a721c286666d
Ryan Endacott <rzeg24@gmail.com>
2013-04-10 16:11:18 -0500

    Cleaned up q2 some

--- ACM2050q1skeleton.c
+++ ACM2050q1skeleton.c
@@ -1,171 +1,188 @@
- /*Challenge
+/*Challenge
  * Write a program that prints out all the words in a string in reverse order
  * For this challenge, a word is defined to be anything delimited by whitespace
  * I.E. "Hello World!" Hello and World! are the two words. & the output should be World! Hello
  * Note, the program should preserve the same amount of white space
  * I.E. "h   i" is different from "h i"
  */
- 
- #include <stdio.h>
- #include <stdlib.h>
- #include <string.h>
 
-    #define MAX_LINE 100
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-    typedef struct list{
-        char data[MAX_LINE + 1];
-        struct list *next;
-    }ListT;
+#define MAX_LINE 100
 
-    //Linked List Utility Functions
-	/*Note the parameters and be sure to pass by ref and val when appropriate */
-    void pop( ListT **head );
-    void push( ListT **head, char *data );
-	void popTail( ListT **head);
-	void pushTail( ListT **head, char *data );
-    void printList( ListT *head );
+typedef struct list{
+  char data[MAX_LINE + 1];
+  struct list *next;
+}ListT;
 
-    int main ( void ){
+//Linked List Utility Functions
+/*Note the parameters and be sure to pass by ref and val when appropriate */
+void pop( ListT **head );
+void push( ListT **head, char *data );
+void popTail( ListT **head);
+void pushTail( ListT **head, char *data );
+void printList( ListT *head );
 
-	/* Variable Declarations, You can have as many as you'd like */
-		/* do not change this assignment */
-		const char *myString = "winner! the are You";
-      
-    
-    printf("\nReverse the following string:\n\n%s \n\n", myString);
+// Strip newline from inputted string
+char *removeNewline(char *s);
 
-	/* Code for challenge goes here */
-	
+// Function you'll fill out.
+char *reverse_words(char *s);
 
-	
-	
-	
-	
-	
-	/* End code for challenge */
-    
-    printf("\n\n\n");
-     
-        return 0;
-    }
+int main ( void ){
 
 
+  char *myString = malloc(sizeof(char) * MAX_LINE);
 
-    void pop( ListT **head ){
+  printf("\nPlease enter a string: ");
+  fgets(myString, MAX_LINE - 1, stdin);
+  myString = removeNewline(myString);
 
-        //Temp variable used for node deletion.
-        ListT *temp = NULL;
-		
-		//Check to see if list is empty
-		if( *head == NULL) return;
+  printf("\nReverse the following string:\n%s\n", myString);
 
-        //Set the temp variable equal to the head and the head equal to its .next field.
-        temp = *head;
-        *head = (*head)->next;
+  myString = reverse_words(myString);
 
-        //Print and free the previous head.
-        printf("%s", temp->data);
-        free(temp);
 
-        return;
-    }
+  printf("The string with the words reversed is:\n%s\n");
 
-    void push( ListT **head, char *data ){
+  free(myString);
 
-        //Malloc space for the new node to be inserted.
-        ListT * newNode = malloc(sizeof(struct list));
+  printf("\n\n\n");
 
-        //Copy data into the new node's data field, and set its .next field to NULL.
-        strcpy(newNode->data, data);
-        newNode->next = NULL;
+  return 0;
+}
 
-        //If the list is empty, set the head equal to the new node.
-        if( *head == NULL ){
-            *head = newNode;
-            return;
-        }
 
-        //Insert the new node into the front of the list.
-        newNode->next = *head;
-        *head = newNode;
 
-        return;
+void pop( ListT **head ){
 
-    }
-	
-	void pushTail( ListT **head, char *data ){
-		
-		ListT * currentNode = ( *head );
-		
-		//Malloc space for the new node
-		ListT * newNode = malloc(sizeof(struct list));
-		
-		//Copy data into th enew node's data feild and set its .next field to NULL
-		strcpy(newNode->data, data);
-		newNode->next = NULL;
-		
-		//If the list is empty, set the head equal to the new node.
-		if( *head == NULL ){
-			*head = newNode;
-			return;
-		}
-		
-		//Iterate to the end of the list
-		//When the end is reached, append the new node
-		while( currentNode->next != NULL ) 
-			currentNode = currentNode->next;
-			
-		currentNode->next = newNode;
-		
-		return;
+  //Temp variable used for node deletion.
+  ListT *temp = NULL;
 
-	}
-	
-	void popTail( ListT **head){
-	
-		//Temp variable used for node deletion & current variable for iteration.
-        ListT * temp = NULL;
-		ListT * currentNode = *head;
-		ListT * previousNode = NULL;
-		
-		//If the list is empty
-		if( *head == NULL ) return;
-		
-		//If the list consist of only the head
-		if( (*head)->next == NULL ){
-			printf("%s" ,(*head)->data);
-			*head = NULL;
-			return;
-		}
-		
-		//Set previous to head since it was already checked
-		//Set current to head->next
-		previousNode = *head;
-		currentNode = (*head)->next;
-		
-		
-		//Iterate to the end of the list
-		while( currentNode->next != NULL ){
-			previousNode = currentNode;
-			currentNode = currentNode->next;
-		}
-		
-		//free the current node and set the previous .next field to NULL
-		temp = currentNode;
-		previousNode->next = NULL;
-        printf("%s", temp->data);
-        free(temp);
-	
-	}
-	
+  //Check to see if list is empty
+  if( *head == NULL) return;
 
-    void printList( ListT *head ){
+  //Set the temp variable equal to the head and the head equal to its .next field.
+  temp = *head;
+  *head = (*head)->next;
 
-        while(head != NULL){
-            printf("%s\n", head->data);
-            head = head->next;
-        }
+  //Print and free the previous head.
+  printf("%s", temp->data);
+  free(temp);
 
-        return;
+  return;
+}
 
-    }
+void push( ListT **head, char *data ){
+
+  //Malloc space for the new node to be inserted.
+  ListT * newNode = malloc(sizeof(struct list));
+
+  //Copy data into the new node's data field, and set its .next field to NULL.
+  strcpy(newNode->data, data);
+  newNode->next = NULL;
+
+  //If the list is empty, set the head equal to the new node.
+  if( *head == NULL ){
+    *head = newNode;
+    return;
+  }
+
+  //Insert the new node into the front of the list.
+  newNode->next = *head;
+  *head = newNode;
+
+  return;
+
+}
+
+void pushTail( ListT **head, char *data ){
+
+  ListT * currentNode = ( *head );
+
+  //Malloc space for the new node
+  ListT * newNode = malloc(sizeof(struct list));
+
+  //Copy data into the new node's data field and set its .next field to NULL
+  strcpy(newNode->data, data);
+  newNode->next = NULL;
+
+  //If the list is empty, set the head equal to the new node.
+  if( *head == NULL ){
+    *head = newNode;
+    return;
+  }
+
+  //Iterate to the end of the list
+  //When the end is reached, append the new node
+  while( currentNode->next != NULL ) 
+    currentNode = currentNode->next;
+
+  currentNode->next = newNode;
+
+  return;
+
+}
+
+void popTail( ListT **head){
+
+  //Temp variable used for node deletion & current variable for iteration.
+  ListT * temp = NULL;
+  ListT * currentNode = *head;
+  ListT * previousNode = NULL;
+
+  //If the list is empty
+  if( *head == NULL ) return;
+
+  //If the list consist of only the head
+  if( (*head)->next == NULL ){
+    printf("%s" ,(*head)->data);
+    *head = NULL;
+    return;
+  }
+
+  //Set previous to head since it was already checked
+  //Set current to head->next
+  previousNode = *head;
+  currentNode = (*head)->next;
+
+
+  //Iterate to the end of the list
+  while( currentNode->next != NULL ){
+    previousNode = currentNode;
+    currentNode = currentNode->next;
+  }
+
+  //free the current node and set the previous .next field to NULL
+  temp = currentNode;
+  previousNode->next = NULL;
+  printf("%s", temp->data);
+  free(temp);
+
+}
+
+
+void printList( ListT *head ){
+
+  while(head != NULL){
+    printf("%s\n", head->data);
+    head = head->next;
+  }
+
+  return;
+
+}
+
+
+char *removeNewline(char *s)
+{
+  int len = strlen(s);
+
+  if (len > 0 && s[len-1] == '\n')  // if there's a newline
+    s[len-1] = '\0';          // truncate the string
+
+  return s;
+}
+
