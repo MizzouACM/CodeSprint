c2a8b657736eef863b6ae4136a98e12eb1e7d190
Ryan Endacott <rzeg24@gmail.com>
2013-04-10 15:59:57 -0500

    Fixed indentation for skeleton 1

--- ACM1050q1skeleton.c
+++ ACM1050q1skeleton.c
@@ -2,36 +2,38 @@
 *	 Given an integer, print the number out in reverse, WITHOUT converting it to a string.
 *    Within that same function, determine if the number is a palindrome
 *  --Hint, while printing in reverse, keep track of the highest place value in the number
+*  Fill in the reversePrintPal function.
 */
 
-	#include <stdio.h>
-	#include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+// Function reversePrintPal accepts a number and prints it out in reverse
+void reversePrintPal( int number );
+
+
+int main ( void ){
 	
-	// Function reversePrintPal accepts a number and prints it out in reverse
-	void reversePrintPal( int number );
+	// Variable to hold user input
+	int num1 = 0;
+
+	// Request a number and print it out
+	printf( "\nEnter Number:  " );
+	scanf( "%d", &num1 );
+	printf( "\n\nThe number before reversing: %d\n\n", num1 );
 	
+	// Call function reversePrint;
+	printf( "\n\nThe number after reversing: " );
+	reversePrintPal( num1 );
+	printf( "\n\n" );
+
+	return 0;
+}
+
+
+// Only touch this function.
+void reversePrintPal( int number ){
+
+	// Your code here:
 	
-		int main ( void ){
-			
-			// Variable to hold user input
-			int num1 = 0;
-		
-			// Request a number and print it out
-			printf( "\nEnter Number:  " );
-			scanf( "%d", &num1 );
-			printf( "\n\nThe number before reversing: %d\n\n", num1 );
-			
-			// Call function reversePrint;
-			printf( "\n\nThe number after reversing: " );
-			reversePrintPal( num1 );
-			printf( "\n\n" );
-		
-			return 0;
-		}
-		
-		
-		void reversePrintPal( int number ){
-			
-			
-			
-		}
+}
